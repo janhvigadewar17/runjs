c2e3772298eb19e6fe369f0d5f2768cc3d55b922
Janhvi Gadewar <85011463+janhvigadewar17@users.noreply.github.com>
2023-10-09 16:37:49 +0000

    add - datatype.js

---
+++ basics/datatypes.js
@@ -0,0 +1,15 @@
+"use strict" // to use the newer version of js
+
+let state;
+
+// number -> 2 to power 53
+// bigint -> 
+// string -> ""
+// boolean -> true/false
+// null -> standalone value, typeof is object
+// undefined -> value not yet assigned, let state; typeof is undefined
+// symbol -> unique, used in react to identify the components, also used in figma
+
+//object
+
+
