4744e6e9354ec0e182d16720d82a548bbe1eb1da
Janhvi Gadewar <85011463+janhvigadewar17@users.noreply.github.com>
2023-10-09 17:03:31 +0000

    conversion of Datatypes

---
+++ basics/conversionOperaitons.js
@@ -0,0 +1,25 @@
+// a nightmare topic
+let score = "33abc";
+console.log(typeof score);
+
+//lets say while filling a form a user had submitted string in case of  a field but it was required to be a number, then u can convert it into a number
+let valueIntNumber = Number(score);
+console.log(typeof valueIntNumber);
+console.log(valueIntNumber);
+//As there is a  bug in javascript therefore we just cant rely on it , hence typescript is used
+
+//basic number conversion
+// "33" -> 33
+// "33abc" -> NaN
+// true -> 1; 0 -> false
+
+// basic boolean conversion
+// "" -> false
+// "hitesh" -> true
+
+// basic string conversion
+
+
+
+
+
