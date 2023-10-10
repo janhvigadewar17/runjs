d9a4182c1819662c60ce248e4030fe4b1007904e
Janhvi Gadewar <85011463+janhvigadewar17@users.noreply.github.com>
2023-10-10 09:03:48 +0000

    add - summary

---
+++ basics/summaryDatatypes.js
@@ -0,0 +1,28 @@
+// Two types of datatypes 
+// primitive and Non-primitive
+ /* *****primitive****8 -> all are call by value, a change happens in the copy itself
+   String 
+   Number
+   Boolean
+   Null
+   Undefined
+   Symbol
+   BigInt
+
+//*******Reference (Non-primitive)******* types -> memory me reference allocate kr skte hai, all of them have return type as function
+
+  Array, Objects, Funcitons
+
+////////Javascript is a dynamically typed language
+  */ 
+
+const heros = ["shaktiman", "naagraj", "doga"];
+
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+        console.log("Hello WOrld");
+ }
